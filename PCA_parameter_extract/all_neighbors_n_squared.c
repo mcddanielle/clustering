f7c98a34d7f246b702bb47b04b08cfd7800aadab
mcddanielle <mcd.danielle@gmail.com>
2019-06-18 10:46:19 -0700

    made the write out of all_neighbors less data intensive

--- PCA_parameter_extract/all_neighbors_n_squared.c
+++ PCA_parameter_extract/all_neighbors_n_squared.c
@@ -1,38 +1,39 @@
-/*1.23.19
- *something is wrong with the data structure, so you aren't putting the data in the array,
- and it is segfaulting...
-... so pick a more stupid, working data structure...
-
-...eventually get this to qsort() your data...
-
+/* 6.10.19 
+ * having some interface problems with this code.  
+ * lets dig in and see what is going wrong
+ *
+ * 1.23.19
+ *
+ * ...eventually get this to qsort() your data...
+ *
  *making an n^2 algorithm to find all neighbor distances.
- *using cluster code since it is good at sucking in smtest data
- 
-
+ *using cluster code since it is good at reading in smtest data
+ *
+ *
  * Revisions log:
  * 1.06.19 printing out r_nn, x_nn, y_nn and calculating mean, <r_nn>
-           \delta r_nn' = r_nn - <r_nn>
-	   sorting from small to large(?)
-
+ *          \delta r_nn' = r_nn - <r_nn>
+ *	   sorting from small to large(?)
+ *
  * 7.22.17 use cell N log N algorithm to find near neighbors, 
-           measure the distances, 
-           look at near neighbors and angles
-           also consider \psi_6
-
-	   also clean up the code to remove the older variables
-	   that are no longer used
-
+ *          measure the distances, 
+ *          look at near neighbors and angles
+ *          also consider \psi_6
+ *
+ *	   also clean up the code to remove the older variables
+ *	   that are no longer used
+ *
  * 5.13.16 default input name "smtest" 
  */
-
 
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
 #include<string.h>
+#include <sys/stat.h>
 
 #define PI 3.14159265359
-#define DEBUG 1
+#define DEBUG 0
 #define VERBOSE 0
 
 FILE *f_debug;
@@ -84,20 +85,20 @@ struct parameters{
 
   int nV;
   int nP;
-  int nV1, nV2;
+  //int nV1, nV2;
   int maxnum;
   
-  int maxnum_small;
-  int maxnum_large;
+  //int maxnum; //_small;
+  //int maxnum_large;
   
-  double radius_small;
-  double radius_large;
+  double radius; //_small;
+  //double radius_large;
   double runforce;
   int runtime;
   //
   double density;
-  double phi2_small;
-  double phi1_big;
+  //double phi2_small;
+  //double phi1_big;
   double pdensity;
   //
   double dt;
@@ -144,7 +145,14 @@ main(int argc,char *argv[])
 
   char ascii_file[120]; //="velocity_data/XV_data_t=";
   char str_time[10];
-    
+
+  //if directory doesn't exist, make directory
+  //make and/or open directory snapshot_files
+  struct stat st = {0};       //null directory
+  if (stat("velocity_data", &st) == -1) {
+    mkdir("velocity_data",0700);
+  }
+  
   //parameters
   double density;
   struct syssize syssize;
@@ -228,11 +236,11 @@ main(int argc,char *argv[])
 
     //cut early frames
     if(!completeframe) break;
-    if(frame==0 || frame==1) continue;
+    if(frame==0) continue;
 
     //open file to write data
     strcpy(ascii_file,"neighbor_data/frame_");
-    sprintf(str_time,"%08d",frame); //convert current to a string   
+    sprintf(str_time,"%08d",time); //convert current to a string   
     strcat(ascii_file,str_time);
     if(DEBUG) printf("printing frame data to %s\n",ascii_file);
 
@@ -323,7 +331,9 @@ void pair_interact(int id1,int id2,
 	   vortex[id2].x,vortex[id2].y,
 	   syssize);
 
-      fprintf(out,"%d\t %d\t %f\t %f\t %f \n",id1, id2, dr,dx,dy);    
+  //just print dr since that is the only analysis we are using.  save data.
+  //fprintf(out,"%d\t %d\t %f\t %f\t %f \n",id1, id2, dr,dx,dy);    
+  fprintf(out,"%f\n",dr);    
 
   //Assign the measured distance to the neighbor arrays in each vortex struct.
   //vortex[id1].neighbor_distance_list[vortex[id1].neighbor_count] = dr;
@@ -373,21 +383,21 @@ void get_parameters_file(struct parameters *parameters,
   fflush(stdout);
 
   fscanf(in,"%s %lf\n",trash,&((*parameters).density));
-  fscanf(in,"%s %lf\n",trash,&((*parameters).phi2_small));
-  fscanf(in,"%s %lf\n",trash,&((*parameters).phi1_big));
+  //fscanf(in,"%s %lf\n",trash,&((*parameters).phi2_small));
+  //fscanf(in,"%s %lf\n",trash,&((*parameters).phi1_big));
   fscanf(in,"%s %lf\n",trash,&((*parameters).pdensity));
 
   //----------------------------------------------------------------------
   //check whether the user gave you sensible choices for the two densities
   //----------------------------------------------------------------------
-  double test1 = (*parameters).phi2_small + (*parameters).phi1_big;
-  if( fabs( ((*parameters).density-test1) >0.01) ){
-    printf("Inconsistency in requested densities.\n");
-    printf("Total density: %f\n", (*parameters).density);
-    printf("Small disk density: %f\n", (*parameters).phi2_small);
-    printf("Large disk density: %f\n", (*parameters).phi1_big);
-    exit(-1);
-  }
+  //double test1 = (*parameters).phi2_small + (*parameters).phi1_big;
+  //if( fabs( ((*parameters).density-test1) >0.01) ){
+  //  printf("Inconsistency in requested densities.\n");
+  //  printf("Total density: %f\n", (*parameters).density);
+  //  printf("Small disk density: %f\n", (*parameters).phi2_small);
+  //  printf("Large disk density: %f\n", (*parameters).phi1_big);
+  //  exit(-1);
+  //}
   //---------------------------------------------------------------------
   
   if (VERBOSE) printf("reading parameters density %f, pin density %f.\n", (*parameters).density, (*parameters).pdensity);
@@ -400,17 +410,18 @@ void get_parameters_file(struct parameters *parameters,
   fscanf(in,"%s %lf\n",trash,&((*syssize).SY));
   (*syssize).SY2=(*syssize).SY*0.5;
   
-  fscanf(in,"%s %lf\n",trash,&((*parameters).radius_small));
-  fscanf(in,"%s %lf\n",trash,&((*parameters).radius_large));
+  fscanf(in,"%s %lf\n",trash,&((*parameters).radius)); //_small));
+  //fscanf(in,"%s %lf\n",trash,&((*parameters).radius_large));
 
   //--where it is appropriate to accurately calculate maxnum
   //--based on particle sizes
 
-  double prefix = (*syssize).SX*(*syssize).SY/PI;
-  double phiS_term = (*parameters).phi2_small/( (*parameters).radius_small*(*parameters).radius_small) ;
-  double phiB_term = (*parameters).phi1_big/( (*parameters).radius_large*(*parameters).radius_large);
+  //double prefix = (*syssize).SX*(*syssize).SY/PI;
+  //double phiS_term = (*parameters).phi2_small/( (*parameters).radius_small*(*parameters).radius_small) ;
+  //double phiB_term = (*parameters).phi1_big/( (*parameters).radius_large*(*parameters).radius_large);
    
-  (*parameters).maxnum= (int) floor(prefix * (phiS_term + phiB_term));
+  //(*parameters).maxnum= (int) floor(prefix * (phiS_term + phiB_term));
+  (*parameters).maxnum= (int) floor( (*syssize).SX*(*syssize).SY/PI  *  (*parameters).density /( (*parameters).radius*(*parameters).radius) );
   
   if(VERBOSE) printf("\n maxnum is: %d\n", (*parameters).maxnum);
   //--
